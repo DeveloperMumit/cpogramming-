237f630fa596e2d92eff6fc5ea0bd57870eb35fc
developermumit <mumit9027@gmail.com>
2022-08-02 20:01:15 +0600

    upper to lower case using libary funchtion

---
+++ day-1/uppertolower.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char upper, lower;
+    printf("Enter any upper case letter : ");
+    scanf("%c", &upper);
+
+    lower = tolower(upper);
+    printf("The lowercase letter is : %c", lower);
+    return 0;
+}
