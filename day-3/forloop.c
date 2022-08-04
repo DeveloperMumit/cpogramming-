bd9160f33cf1a259a37a9a8150b52a8e64955b2a
developermumit <mumit9027@gmail.com>
2022-08-04 17:52:38 +0600

    for loop

---
+++ day-3/forloop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+
+//  for loop
+    int sum = 0;
+    for(int i = 1; i <= 100; i = i + 2){
+        sum =  i;
+        printf("%d \n", sum);
+    }
+
+
+    return 0;
+}
